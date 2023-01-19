9cd9adeb67bd328ad5e33bb39cfa7a69fadb9eea
Carlo Cicchi <carlo.cicchi@hotmail.it>
2023-01-19 20:44:15 +0100

    Data Json Added

---
+++ src/app/classes/interfaces.ts
@@ -0,0 +1,14 @@
+export interface ICake {
+  name: string;
+  price: number;
+  quantity: number;
+  image?: string;
+  ingredients: IIngredient[];
+}
+
+export interface IIngredient {
+  name: string;
+  quantity: number;
+  unit: string;
+}
+
